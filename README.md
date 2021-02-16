be01e6b95fc3eab2bf78f7f797effd90f9da46ad
Daniel Voros <daniel.voros@gmail.com>
2021-02-16 02:04:20 +0100

    asdp v1

---
+++ README.md
@@ -0,0 +1,3 @@
+# point-convert
+
+Utility that converts `.asdp` files to [PCD](https://pointclouds.org/documentation/tutorials/pcd_file_format.html)s.
--- main.go
+++ main.go
@@ -3,15 +3,25 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"html/template"
 	"log"
 	"math"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 )
 
-var cols = 1600
-var rows = 1200
+type Params struct {
+	XResolution int
+	YResolution int
+	NumRows     int
+	NumPoints   uint64
+}
+
+var params = Params{}
+
+var headerLine = regexp.MustCompile(`^#\s*(\S+)+ (\S+)$`)
 
 func convertLine(line string) string {
 	fields := strings.Split(line, ",")
@@ -23,39 +33,82 @@ func convertLine(line string) string {
 	if err != nil {
 		log.Fatal(err)
 	}
-	c = c % cols
-	a := math.Pi * 2.0 * (float64(c) / float64(cols))
+	c = c % params.XResolution
+	a := math.Pi * 2.0 * (float64(c) / float64(params.XResolution))
 
 	r, err := strconv.Atoi(fields[2])
 	if err != nil {
 		log.Fatal(err)
 	}
-	b := math.Pi * 2.0 * (float64(r) / float64(rows))
+	b := math.Pi * 2.0 * (float64(r) / float64(params.YResolution))
 
 	d, err := strconv.ParseFloat(fields[3], 64)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	x := -d * math.Cos(b) * math.Sin(a)
+	x := d * math.Cos(b) * math.Sin(a)
 	y := d * math.Cos(b) * math.Cos(a)
 	z := d * math.Sin(b)
 
 	return fmt.Sprintf("%f %f %f", x/100, y/100, z/100)
 }
 
-func printPCDHeader() {
-	fmt.Println(`# .PCD v.7 - Point Cloud Data file format
+func printPCDHeader(p Params) {
+	p.NumPoints = (uint64)(p.NumRows) * (uint64)(p.XResolution)
+	tmpl, err := template.New("pcd-header").Parse(`# .PCD v.7 - Point Cloud Data file format
 VERSION .7
 FIELDS x y z
 SIZE 4 4 4
 TYPE F F F
 COUNT 1 1 1
-WIDTH 1600
-HEIGHT 301
+WIDTH {{.XResolution}}
+HEIGHT {{.NumRows}}
 VIEWPOINT 0 0 0 1 0 0 0
-POINTS 481600
-DATA ascii`)
+POINTS {{.NumPoints}}
+DATA ascii
+`)
+	if err != nil {
+		log.Fatalf("unable to parse PCD header template: %v\n", err)
+	}
+	err = tmpl.Execute(os.Stdout, p)
+	if err != nil {
+		log.Fatalf("unable to execute PCD header template: %v\n", err)
+	}
+}
+
+func processHeader(line string) {
+	headerLine.MatchString(line)
+
+	groups := headerLine.FindStringSubmatch(line)
+	if len(groups) != 3 {
+		log.Fatalf("invalid header: %s\n", line)
+	}
+
+	var err error
+	if groups[1] == "version" {
+		if groups[2] != "1" {
+			log.Fatalf("unknown version in header: %s\n", groups[2])
+		}
+	}
+	if groups[1] == "x-resolution" {
+		params.XResolution, err = strconv.Atoi(groups[2])
+		if err != nil {
+			log.Fatalf("invalid header: %s\n", line)
+		}
+	}
+	if groups[1] == "y-resolution" {
+		params.YResolution, err = strconv.Atoi(groups[2])
+		if err != nil {
+			log.Fatalf("invalid header: %s\n", line)
+		}
+	}
+	if groups[1] == "num-rows" {
+		params.NumRows, err = strconv.Atoi(groups[2])
+		if err != nil {
+			log.Fatalf("invalid header: %s\n", line)
+		}
+	}
 }
 
 func convertFile(fileName string) {
@@ -64,12 +117,20 @@ func convertFile(fileName string) {
 		log.Fatal(err)
 	}
 
-	printPCDHeader()
-
 	scanner := bufio.NewScanner(f)
+	firstPoint := true
 	for scanner.Scan() {
-		l := convertLine(scanner.Text())
-		fmt.Println(l)
+		t := scanner.Text()
+		if strings.HasPrefix(t, "#") {
+			processHeader(t)
+		} else {
+			if firstPoint {
+				printPCDHeader(params)
+				firstPoint = false
+			}
+			l := convertLine(scanner.Text())
+			fmt.Println(l)
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
